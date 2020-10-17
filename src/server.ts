a1904e602a2049803d30049808f7567948a00290
wcfx <wcfx.dev@gmail.com>
2020-10-17 19:50:59 -0300

    Criando as entradas da entidade

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.post('/orphanages', (req, res) => {
+
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+
+  });
+
+  orphanagesRepository.save(orphanage);
+
+  return res.json({ message: "Hello World"});
+});
+
+app.listen(3333);
